0347047dbd17be294172245558c9ae0ff80aea41
yaw joseph etse <yaw.etse@gmail.com>
2018-02-21 16:40:13 -0500

    updating coveralls

---
+++ src/pd.js
@@ -0,0 +1,7 @@
+import * as probabilty from 'probability-distributions';
+
+/**
+ * @namespace
+ * @see {@link https://github.com/Mattasher/probability-distributions} 
+ */
+export const PD = Object.assign({}, probabilty);
