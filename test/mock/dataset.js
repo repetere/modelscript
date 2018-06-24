ba238288daf5c9ac3d58aee595627d3f0b72e477
yaw joseph etse <yaw.etse@gmail.com>
2018-06-24 17:44:41 -0400

    added column scaling

---
+++ test/mock/dataset.js
@@ -0,0 +1,61 @@
+exports.fullData = [{
+  'Country': 'Brazil',
+  'Age': '44',
+  'Salary': '72000',
+  'Purchased': 'N',
+},
+{
+  'Country': 'Mexico',
+  'Age': '27',
+  'Salary': '48000',
+  'Purchased': 'Yes',
+},
+{
+  'Country': 'Ghana',
+  'Age': '30',
+  'Salary': '54000',
+  'Purchased': 'No',
+},
+{
+  'Country': 'Mexico',
+  'Age': '38',
+  'Salary': '61000',
+  'Purchased': 'f',
+},
+{
+  'Country': 'Ghana',
+  'Age': '40',
+  'Salary': '73000',
+  'Purchased': 'Yes',
+},
+{
+  'Country': 'Brazil',
+  'Age': '35',
+  'Salary': '58000',
+  'Purchased': 'Yes',
+},
+{
+  'Country': 'Mexico',
+  'Age': '40',
+  'Salary': '52000',
+  'Purchased': 'false',
+},
+{
+  'Country': 'Brazil',
+  'Age': '48',
+  'Salary': '79000',
+  'Purchased': 'Yes',
+},
+{
+  'Country': 'Ghana',
+  'Age': '50',
+  'Salary': '83000',
+  'Purchased': 'No',
+},
+{
+  'Country': 'Brazil',
+  'Age': '37',
+  'Salary': '67000',
+  'Purchased': 'Yes',
+},
+];
